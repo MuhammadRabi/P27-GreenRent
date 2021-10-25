ef8d1c753b8ce0735c5a51f38847e8bca19d378a
Muhammad Rabi <vonrabi07@yahoo.com>
2021-10-25 20:38:18 +0200

    inital commit

---
+++ dist/all.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{autoplay:{delay:5e3},pagination:{el:".swiper-pagination",dynamicBullets:!0,loop:!0}});
+//# sourceMappingURL=all.js.map
---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    loop: true,
+  },
+});
