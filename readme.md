21b083df032658606b0f123a30279d038e0b8a45
Muhammad Rabi <vonrabi07@yahoo.com>
2021-10-28 22:49:38 +0200

    fixing navbar

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 ## Fully responsive landing page
+
+Furniture company website
+idenity color Green #8db904
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// initializing swiper slider
 var swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 5000,
@@ -10,7 +11,6 @@ var swiper = new Swiper(".swiper", {
 });
 
 //mobile nav
-
 const navBar = document.querySelector("nav");
 const navToggle = document.querySelector(".menu-toggle");
 
@@ -18,3 +18,42 @@ navToggle.addEventListener("click", () => {
   navBar.classList.toggle("active");
   navToggle.classList.toggle("active");
 });
+
+// closing Full screen Nav when pressing Escape Button
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    navToggle.classList.remove("active");
+    navBar.classList.remove("active");
+  }
+});
+
+// trial
+
+// scroll to top btn
+
+const scrollToTop = document.querySelector(".scroll-to-top");
+window.addEventListener("scroll", () => {
+  let windowScroll = window.scrollY;
+  windowScroll > 700
+    ? scrollToTop.classList.add("active")
+    : scrollToTop.classList.remove("active");
+
+  let topBar = document.querySelector(".top");
+
+  //if (windowScroll > topBar.offsetTop) {
+  // topBar.style.display = "none";
+  //}
+  //else {
+  //  topBar.style.display = "block";
+  //}
+  toTop(scrollToTop);
+});
+
+const toTop = (btn) => {
+  btn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+};
